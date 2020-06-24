754f70f87129a65edbd5a68ee7a820b0699d1b0d
ArifCengic <cengica@hotmail.com>
2020-06-24 09:09:04 -0400

    Created requirements.txt and Added Redis code to view.py

--- articles/views.py
+++ articles/views.py
@@ -19,19 +19,20 @@ with open('content_api.json',) as f:
     articles = data['results']
     for i, a in enumerate(articles):
         print(f" {i} - {a['headline']}")
-        # Convert string publish_at to datetime so it can be easily formated
-        # if type(a['publish_at']) == str:
-        #     a['publish_at'] = datetime.datetime.strptime(
-        #                         a['publish_at'],
-        #                         "%Y-%m-%dT%H:%M:%S%z")
 
         if (redis_instance.llen('ArticlesList') < 10):
+            # we use Redis list to store values for index based random selection
+            # and Redis hash to lookup Articles by 'uuid' - assuming its unique i
+            # TODO replace redis list with hash (key-value) and set
+            # use SRANDMEMBER key [count] to get count random keys 
+            # redis_instance.sadd("QuotesSet", json.dumps(q))
             redis_instance.lpush('ArticlesList', json.dumps(a))
+            redis_instance.hset('ArticlesHash', a['uuid'], json.dumps(a))
 
             for t in a['tags']:
                 if t['slug'] == "10-promise":
                     redis_instance.lpush('ArticlesPromiseList', json.dumps(a))
-                # articles_promise_slug.append(a)
+                
 
 quotes = []
 with open('quotes_api.json',) as f:
@@ -40,15 +41,18 @@ with open('quotes_api.json',) as f:
         print(f"Adding {len(quotes)} Quotes to Redis")
         for q in quotes:
             redis_instance.lpush('QuotesList', json.dumps(q))
-
-
+            
+def convert_str_date(a, key):
+    # For Articles need to convert date string to datetime 
+    # so it can be formated as date and time in Django Views
+    if key in a and type(a[key]) == str:
+            a[key] = datetime.datetime.strptime(
+                                a[key],
+                                "%Y-%m-%dT%H:%M:%S%z")
 
 def get_random_list(arr, count):
 
     total = redis_instance.llen(arr)
-    # if (len(arr) == 0 or count > len(arr)):
-    #     return []
-
     random_indexes = []
 
     while len(random_indexes) < count:
@@ -59,15 +63,10 @@ def get_random_list(arr, count):
     result = []
     
     for i in random_indexes:
+        # get string val from Redis list convert to JSON
         value = redis_instance.lrange(arr,i,i)[0]
         a = json.loads(value.decode('utf-8'))
-
-        # For ArticlesList Convert string publish_at to datetime 
-        # so it can be easily formated in Django Views
-        if arr == "ArticlesList" and type(a['publish_at']) == str:
-            a['publish_at'] = datetime.datetime.strptime(
-                                a['publish_at'],
-                                "%Y-%m-%dT%H:%M:%S%z")
+        convert_str_date(a, 'publish_at')
         result.append(a)
     
     return result
@@ -82,17 +81,19 @@ def article(request, article_id):
                         article_id=article_id)
             c.save()
         else:
-            return HttpResponse(f"Got your comment {cf.cleaned_data['text']} on Article ID {article_id}.")
+            #TODO check fo faul language, curse words etc in comment text
+            return HttpResponse(f"Problem with your comment {cf.cleaned_data['text']} on Article ID {article_id}.")
 
     try:
-        article = next(a for a in articles if a['uuid'] == article_id)
-    except StopIteration:
+        value = redis_instance.hget('ArticlesHash', article_id)
+        article = json.loads(value)
+        convert_str_date(article, 'publish_at')
+    except:
         raise Http404(f"Can not find Article {article_id}")
 
     comments = Comment.objects.filter(article_id=article_id).order_by('pub_date')
 
     return render(request, 'articles/article.html', {
-        # 'stocks': get_random_list(quotes, 3),
         'stocks': get_random_list("QuotesList", 3),
         'article': article,
         'comments': comments,
